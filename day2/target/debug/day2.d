267e81a7c99c6e0e7482a5ca6294c133df39155c
flber <benrhammond@gmail.com>
2020-12-02 00:32:47 -0800

    some number's the charm...

---
+++ day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+/home/benh/Documents/AoC20/day2/target/debug/day2: /home/benh/Documents/AoC20/day2/src/main.rs
---
+++ day2/target/debug/deps/day2-8685c3fb5785a7fc.d
@@ -0,0 +1,5 @@
+/home/benh/Documents/AoC20/day2/target/debug/deps/day2-8685c3fb5785a7fc: src/main.rs
+
+/home/benh/Documents/AoC20/day2/target/debug/deps/day2-8685c3fb5785a7fc.d: src/main.rs
+
+src/main.rs:
